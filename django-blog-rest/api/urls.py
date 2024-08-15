2f56f6e20a556afd9444e2e4c7c1a3d9d8ee98a8
omernawaz <omer.nawaz@arbisoft.com>
2024-08-15 17:15:41 +0500

    [Added] Endpoint for refreshing tokens with rotating tokens and blacklisting rotated tokens

--- django-blog-rest/api/urls.py
+++ django-blog-rest/api/urls.py
@@ -1,15 +1,20 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 from . import views
 
 urlpatterns = [
     path('users/', views.UserView.as_view()),
-    path('users/register/', views.UserRegister.as_view()),
-    path('users/login/', views.UserLogin.as_view()),
-    path('users/logout/', views.UserLogout.as_view()),
+
     path('users/follow/', views.UserFollow.as_view()),
     path('users/<str:username>/', views.UserView.as_view()),
+    
+    path('auth/register/', views.UserRegister.as_view()),
+    path('auth/login/', views.UserLogin.as_view()),
+    path('auth/logout/', views.UserLogout.as_view()),
+    path('auth/refresh/', TokenRefreshView.as_view()),
     
     path('posts/', views.PostView.as_view()),
     path('posts/<int:post_id>/', views.PostView.as_view()),
     path('posts/favourite/', views.PostFavourite.as_view()),
+    
 ]
--- django-blog-rest/django_rest_api/settings.py
+++ django-blog-rest/django_rest_api/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -32,6 +33,10 @@ CORS_ALLOW_CREDENTIALS = True
 
 SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('Bearer',),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ROTATE_REFRESH_TOKENS": True,
+    "BLACKLISH_AFTER_ROTATION": True, 
 }
 
 # Application definition
