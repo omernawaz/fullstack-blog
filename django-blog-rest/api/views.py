f8a5a5e09f67044e400425a6dab0891480a64b8a
omernawaz <omer.nawaz@arbisoft.com>
2024-08-16 17:43:05 +0500

    [Modify] Made Changes to API:  - Cookies are no longer set/deleted. The Custom Auth function will still try to check for cookies, will fix later  - A proper 401 with a JSON response is recieved when a user is not found when trying to log in

--- django-blog-rest/api/views.py
+++ django-blog-rest/api/views.py
@@ -81,10 +81,11 @@ class UserLogin(APIView):
         username = request.data['username']
         password = request.data['password']
         
-        user = User.objects.get(username=username)
-        
-        if not user:
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
             raise AuthenticationFailed("User not found!")
+            
         if not user.check_password(password):
             raise AuthenticationFailed("Incorrect Password")
         
@@ -94,8 +95,10 @@ class UserLogin(APIView):
             'access': str(refresh.access_token),
             'user': UserSerializer(user).data,
         })
-        response.set_cookie(key="access_token", value=response.data['access'], httponly=True)
-        response.set_cookie(key="refresh_token", value=response.data['refresh'], httponly=True)
+        
+        #Disabling this feature, the api will require headers
+        #response.set_cookie(key="access_token", value=response.data['access'], httponly=True)
+        #response.set_cookie(key="refresh_token", value=response.data['refresh'], httponly=True)
         
         return response
 
@@ -120,8 +123,11 @@ class UserLogout(APIView):
     permission_classes = [IsAuthenticated]
     def post(self,request):
         response = Response()
-        response.delete_cookie('access_token')
-        response.delete_cookie('refresh_token')
+        
+        #This feature is disabled refer to UserLogin() view
+        #response.delete_cookie('access_token')
+        #response.delete_cookie('refresh_token')
+        
         response.data = {
             'detail': "User logged out successfully",
         }
